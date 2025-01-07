e1af4a5648261c9bd3dbe7e0b47aa643a854abb5
Matt Staab <mstaab16@gmail.com>
2025-01-07 09:24:54 -0600

    colorscheme now actually defaults to gruvbox

--- init.lua
+++ init.lua
@@ -854,8 +854,9 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
     'ellisonleao/gruvbox.nvim',
+    -- 'folke/tokyonight.nvim',
+
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
